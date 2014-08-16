64ce9d9ec977cb47db520f7a773bf3437109de0e
raiot <renedurazo@gmail.com>
2014-08-16 15:42:46 -0700

    [add] repositorio estado cuenta

---
+++ App_Code/Repositorios/RepositorioEstadoDeCuenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de RepositorioEstadoDeCuenta
+/// </summary>
+public class RepositorioEstadoDeCuenta
+{
+    private readonly SICMST2Entities context;
+	public RepositorioEstadoDeCuenta()
+	{
+        context = new SICMST2Entities();
+	}
+
+    public async Task<List<T>> ObtenerQueryGenerico<T>(string query)
+    {
+        return await context.Database.SqlQuery<T>(query).ToListAsync();
+    }
+}
